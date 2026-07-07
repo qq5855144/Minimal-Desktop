daaf460a3d33c5203b5a08fb93fab22c3bab9fd7
练习时长两年半的水怪 <140933838+qq5855144@users.noreply.github.com>
2026-07-07 18:01:48 +0800

    fix: 白屏修复 - BrowserRouter 加 basename、添加 404.html SPA 重定向

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ import { routes } from './routes';
 const App: React.FC = () => {
   return (
     <DesktopProvider>
-      <Router>
+      {/* basename 使用 Vite 注入的 BASE_URL，开发时为 '/'，生产时为 '/Minimal-Desktop/' */}
+      <Router basename={import.meta.env.BASE_URL}>
         <IntersectObserver />
         <Routes>
           {routes.map((route, index) => (
